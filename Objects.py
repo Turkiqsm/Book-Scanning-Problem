feb823a5ab5ad398bf14973bd14e2c9cdf15fcbc
khaled <khaled.hm@outlook.com>
2020-02-25 14:19:03 +0300

    adding Book and Library Objects + reading file and mapping to objects

---
+++ Objects.py
@@ -0,0 +1,42 @@
+class Book(object):
+
+    def __init__ (self, id, score):
+        self.id = id
+        self.score = score
+
+    def getScore(self):
+        return self.score
+
+    def toString(self):
+        print("Book ", self.id , " has a score of", self.score)
+
+
+
+class Library:
+
+    books = []
+    totalScore = 0
+    def __init__(self, id, signUpTime, numBooks, scanningLimit):
+        self.id = id
+        self.signUpTime = signUpTime
+        self.numBooks = numBooks
+        self.scanningLimit = scanningLimit
+        self.books.clear()
+
+    def addBook(self, book):
+        self.books.append(book)
+        self.totalScore += book.score
+        self.books.sort(key=self.sortBooks, reverse=True)
+
+    def sortBooks(self, book):
+        return book.score
+
+
+    def toString(self):
+        print("Library", self.id, "have", self.numBooks, "books, It needs",
+              self.signUpTime, " days to SignUp and it can scan",
+              self.scanningLimit, "books per day")
+        print("Total Score for this Library is", self.totalScore)
+        for b in self.books:
+            b.toString()
+        print("###############################")
